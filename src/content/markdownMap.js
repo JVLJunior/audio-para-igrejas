3bb572db1652ad42c1998f4feac9f11ba024ff0f
JVLJunior <jvljunior2206@gmail.com>
2025-07-01 20:07:25 -0300

    Corrige nome do arquivo markdownMap.js

---
+++ src/content/markdownMap.js
@@ -0,0 +1,20 @@
+// src/content/markdownMap.js
+import introducao from './introducao.md?raw'
+import fundamentosSom from './fundamentos-som.md?raw'
+import microfones from './microfones.md?raw'
+import equalizacao from './equalizacao.md?raw'
+import mesasSom from './mesas-som.md?raw'
+import altoFalantes from './alto-falantes.md?raw'
+import acusticaIgrejas from './acustica-igrejas.md?raw'
+import feedbackControle from './feedback-controle.md?raw'
+
+export const markdownMap = {
+  'introducao.md': introducao,
+  'fundamentos-som.md': fundamentosSom,
+  'microfones.md': microfones,
+  'equalizacao.md': equalizacao,
+  'mesas-som.md': mesasSom,
+  'alto-falantes.md': altoFalantes,
+  'acustica-igrejas.md': acusticaIgrejas,
+  'feedback-controle.md': feedbackControle
+}
